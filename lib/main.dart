7bef0f4622a2371e4ccbf9285ba84cc6433d525d
Naipawat Poolsawat <tata-naipawat@hotmail.com>
2023-04-18 14:43:10 +0700

    Change from fixed fontSize (HomePage) => responsive fontSize

--- lib/main.dart
+++ lib/main.dart
@@ -379,7 +379,6 @@ class BigCard extends StatelessWidget {
     final theme = Theme.of(context);
     final style = theme.textTheme.displayMedium!.copyWith(
       color: theme.colorScheme.onPrimary,
-      fontSize: 70,
     );
     return Card(
       color: theme.colorScheme.secondary,
