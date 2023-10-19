8f80dd8394b8c2ac0cec61eb48b17576c6c07e78
boraxpr <tata-naipawat@hotmail.com>
2023-10-19 17:20:09 +0700

    Add Biometric Authentication Page (Local Auth), Using FutureBuilder to catch Auth snapshot and reflects isAuthenticated with two different icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 ﻿import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:window_manager/window_manager.dart';
 import 'package:camera/camera.dart';
@@ -26,11 +27,7 @@ Future<void> main() async {
   if (cameras.isNotEmpty) {
     final firstCamera = cameras.first;
     //Run MyApp
-    runApp(
-      MyApp(
-        camera: firstCamera,
-      ),
-    );
+    runApp(MyApp(camera: firstCamera));
   } else {
     runApp(NoCamera());
   }
@@ -56,10 +53,84 @@ class _MyAppState extends State<MyApp> {
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
-        home: MyHomePage(
+        home: Auth(
           camera: widget.camera,
         ),
         debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
+
+class Auth extends StatefulWidget {
+  Auth({super.key, required this.camera});
+  final CameraDescription camera;
+
+  @override
+  State<Auth> createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  final LocalAuthentication _localAuthentication = LocalAuthentication();
+
+  Future<bool> _authenticate() async {
+    bool authenticated = false;
+
+    try {
+      authenticated = await _localAuthentication.authenticate(
+        localizedReason: 'Authenticate to access the app',
+        options: const AuthenticationOptions(
+          useErrorDialogs: true,
+          stickyAuth: true,
+          biometricOnly:
+              true, // Set to true to use only biometrics (Face ID/Touch ID),
+        ),
+      );
+    } catch (e) {
+      print('Error during authentication: $e');
+    }
+
+    if (authenticated) {
+      print('User authenticated successfully');
+      // Use the captured context variable
+    } else {
+      print('Authentication failed');
+    }
+    return authenticated;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello Flutter'),
+      ),
+      body: Center(
+        child: IconButton(
+          onPressed: () async {
+            bool isAuthenticated = await _authenticate();
+            if (isAuthenticated) {
+              if (context.mounted) {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MyApp(camera: widget.camera)));
+              }
+            }
+          },
+          icon: FutureBuilder<bool>(
+            future: _authenticate(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else {
+                return Icon(
+                  snapshot.data == true ? Icons.lock_open : Icons.lock,
+                );
+              }
+            },
+          ),
+        ),
       ),
     );
   }
